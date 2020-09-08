54bac4f08b5529682905d65ea6999abd0b983c88
junji-fukuyama <junji.fukuyama@di-v.co.jp>
2020-09-08 22:37:05 +0900

    ユーザー登録(2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,21 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true, uniqueness: { case_sensitive: true }
   validates :birth_date, presence: true
-
+  
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
     validates :first_name
     validates :last_name
   end
-  
-  with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: 'Full-width katakana characters' } do
+
+  with_options presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters' } do
     validates :first_name_kana
     validates :last_name_kana
   end
 
   has_many :items
-  has_many :item_trasactions
+  has_many :item_transactions
+         
 end
